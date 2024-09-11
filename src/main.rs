4d30555b3d8035a931a6970a61610f713aef1a23
Karn Wong <karn@karnwong.me>
2024-09-11 19:29:58 +0700

    set dynamic hostname

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,21 @@ async fn greet(name: web::Path<String>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // init logger
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
+    // set hostname
+    let mut listen_address = "";
+    match MODE {
+        "production" => listen_address = "0.0.0.0",
+        "development" => listen_address = "127.0.0.1",
+        _ => {
+            error!("Please specify env MODE");
+            std::process::exit(1);
+        }
+    }
+
+    // init server
     HttpServer::new(|| App::new().wrap(Logger::default()).service(greet))
         .bind(("127.0.0.1", 8080))?
         .run()
