eee777b0cb01afa840c9b4752ef0e0d60231c480
Karn Wong <karn@karnwong.me>
2024-09-11 19:08:28 +0700

    add logger middleware

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
 use actix_web::{get, web, App, HttpServer, Responder};
 use dotenv_codegen::dotenv;
 use env_logger::Env;
-use log::{debug, error, info, log_enabled, Level};
+// use log::{debug, error, info, log_enabled, Level};
+use actix_web::middleware::Logger;
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
     println!("{}", dotenv!("MODE"));
-    info!("Info!!!");
+    // info!("Info!!!");
     format!("FFF {name}!")
-}
-
-#[get("/foo/{i}")]
-async fn foo(i: web::Path<String>) -> impl Responder {
-    format!("AAA {i}!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
 
-    HttpServer::new(|| App::new().service(greet).service(foo))
+    HttpServer::new(|| App::new().wrap(Logger::default()).service(greet))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
