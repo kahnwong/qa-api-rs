edb1579067c056a2a4619b1424a63b02afcf3ef6
Karn Wong <karn@karnwong.me>
2024-09-11 20:33:33 +0700

    use tracing

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,36 @@
 use actix_web::middleware::Logger;
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, post, web, App, Error, HttpResponse, HttpServer, Responder};
 use dotenv_codegen::dotenv;
-use env_logger::Env;
-use log::{debug, error, info, log_enabled, Level};
+use serde::{Deserialize, Serialize};
 
 const MODE: &str = dotenv!("MODE");
 const QA_API_KEY: &str = dotenv!("QA_API_KEY");
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SubmitRequest {
+    request_id: String,
+    query: String,
+}
 
 #[get("/")]
 async fn root() -> impl Responder {
     "Welcome to qa-api-rs"
 }
 
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    println!("{}", MODE);
-    info!("Info!!!");
-    format!("FFF {name}!")
+#[post("/submit")]
+async fn submit(body: web::Bytes) -> Result<HttpResponse, Error> {
+    log::info!("Info!!!");
+
+    let obj = serde_json::from_slice::<SubmitRequest>(&body)?;
+    // println!("{:?}", obj.query);
+
+    Ok(HttpResponse::Ok().json(obj))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // init logger
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    tracing_subscriber::fmt().json().init();
 
     // set hostname
     let listen_address;
@@ -30,7 +38,7 @@ async fn main() -> std::io::Result<()> {
         "production" => listen_address = "0.0.0.0",
         "development" => listen_address = "127.0.0.1",
         _ => {
-            error!("Please specify env MODE");
+            log::error!("Please specify env MODE");
             std::process::exit(1);
         }
     }
@@ -40,9 +48,9 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(Logger::default())
             .service(root)
-            .service(greet)
+            .service(submit)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((listen_address, 8080))?
     .run()
     .await
 }
