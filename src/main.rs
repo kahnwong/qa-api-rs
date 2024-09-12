c734af4d807387aabddbc0fbd94a1c94ae8e398e
Karn Wong <karn@karnwong.me>
2024-09-12 13:49:48 +0700

    extract routes to routes.rs

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,16 @@
 mod llm;
-use crate::llm::llm_call;
+mod routes;
+
+use crate::routes::{root, submit};
+
 use axum::{
-    http::StatusCode,
     routing::{get, post},
-    Json, Router,
+    Router,
 };
 use dotenv_codegen::dotenv;
-use serde::{Deserialize, Serialize};
 
 const MODE: &str = dotenv!("MODE");
 // const QA_API_KEY: &str = dotenv!("QA_API_KEY");
-
-#[derive(Deserialize)]
-struct SubmitRequest {
-    request_id: String,
-    query: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct SubmitResponse {
-    request_id: String,
-    query: String,
-    response: String,
-}
-
-async fn root() -> &'static str {
-    "Welcome to qa-api-rs"
-}
-
-async fn submit(Json(payload): Json<SubmitRequest>) -> (StatusCode, Json<SubmitResponse>) {
-    // get answer
-    let answer = llm_call(&payload.query).await.unwrap();
-
-    // return response
-    let response = SubmitResponse {
-        request_id: payload.request_id,
-        query: (&payload.query).to_string(),
-        response: answer,
-    };
-
-    log::info!("{}", serde_json::to_string(&response).unwrap());
-
-    (StatusCode::OK, Json(response))
-}
 
 #[tokio::main]
 async fn main() {
---
+++ src/routes.rs
@@ -0,0 +1,37 @@
+use crate::llm::llm_call;
+use axum::http::StatusCode;
+use axum::Json;
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+pub struct SubmitRequest {
+    request_id: String,
+    query: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SubmitResponse {
+    request_id: String,
+    query: String,
+    response: String,
+}
+
+pub async fn root() -> &'static str {
+    "Welcome to qa-api-rs"
+}
+
+pub async fn submit(Json(payload): Json<SubmitRequest>) -> (StatusCode, Json<SubmitResponse>) {
+    // get answer
+    let answer = llm_call(&payload.query).await.unwrap();
+
+    // return response
+    let response = SubmitResponse {
+        request_id: payload.request_id,
+        query: (&payload.query).to_string(),
+        response: answer,
+    };
+
+    log::info!("{}", serde_json::to_string(&response).unwrap());
+
+    (StatusCode::OK, Json(response))
+}
